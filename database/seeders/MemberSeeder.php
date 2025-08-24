b8be640df2658c1b12d9c5335c0ab8a72475bf46
Rafiaf15 <rafiabdulfatah9@gmail.com>
2025-08-24 12:57:24 +0700

    Revert "update database"
    This reverts commit bfa32e4d32091210d6666bdb0308617822120e9b.

--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -9,32 +9,13 @@ class MemberSeeder extends Seeder
 {
     public function run(): void
     {
+        // Contoh data: memakai salah satu nama dari gambar (misal: BUDI SANTOSO)
         Member::firstOrCreate(
-            ['name' => 'Pancar Anugrah Sejati, S.KOM.'],
+            ['name' => 'BUDI SANTOSO'],
             [
-                'rank' => 'IPDA',
-                'nrp' => '9908174',
-                'position' => 'PS. PAMIN SUBBAGMONEV BAGJEMEN TIK DIV TIK POLRI',
-                'annual_quota' => 12,
-            ]
-        );
-
-        Member::firstOrCreate(
-            ['name' => 'DOBY PRAYADINATA, S.T., M.T. '],
-            [
-                'rank' => 'AKBP ',
-                'nrp' => '80061251',
-                'position' => 'KASUBBAGKAMDATIN BAGKELOLAKAMDATIN RODATIN DIV TIK POLRI',
-                'annual_quota' => 12,
-            ]
-        );
-        
-        Member::firstOrCreate(
-            ['name' => 'KARTIKA WIDYASTUTI, S.Kom.'],
-            [
-                'rank' => 'Penata',
-                'nrp' => '197902272008122001 ',
-                'position' => 'PS. KAURTU RODATIN DIV TIK POLRI',
+                'rank' => 'KOMBES POL',
+                'nrp' => '67120573',
+                'position' => 'KABAG APLIKASI ROTEKINFO DIV TIK POLRI',
                 'annual_quota' => 12,
             ]
         );
